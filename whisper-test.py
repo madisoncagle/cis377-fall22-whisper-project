207adaf90e5b437102ff12262718eaa1fdbbe9f9
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2022-11-28 17:56:01 -0600

    Run tests on DARPA TIMIT dataset

--- whisper-test.py
+++ whisper-test.py
@@ -1,5 +1,18 @@
-import whisper
+import os
+import random
 
-model = whisper.load_model("base")
-result = model.transcribe("afrikaans1.mp3")
-print(result["text"])
+recs = {}
+
+for path, _, files in os.walk('./darpa-timit/data/TEST/'):
+    for file in files:
+        if str(file).endswith(".wav"):
+            f = open(os.path.join(path, file).replace('\\', '/').replace('WAV.wav', 'TXT'))
+            txt = f.readline()
+            recs[os.path.join(path, file).replace('\\', '/')] = txt[8:-1]
+
+clips = []
+
+for i in range(100):
+    clips.append(random.choice(list(recs.items())))
+
+test = [clips[i][1] for i in range(len(clips))]
