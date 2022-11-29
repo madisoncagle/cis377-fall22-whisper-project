63094e0d5bbf19ab15db1ce158e076c1f9f7abb6
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2022-11-29 08:07:10 -0600

    Add ravdess-song-test.ipynb, remove duplicate .wav files
    Edit whisper-test.py to test code before putting in the notebook

--- whisper-test.py
+++ whisper-test.py
@@ -3,16 +3,19 @@ import random
 
 recs = {}
 
-for path, _, files in os.walk('./darpa-timit/data/TEST/'):
+for path, _, files in os.walk('./ravdess-song/'):
     for file in files:
-        if str(file).endswith(".wav"):
-            f = open(os.path.join(path, file).replace('\\', '/').replace('WAV.wav', 'TXT'))
-            txt = f.readline()
-            recs[os.path.join(path, file).replace('\\', '/')] = txt[8:-1]
+        if file[12:14] == "01": # parse file name to get sentence
+            txt = "Kids are talking by the door"
+        else:
+            txt = "Dogs are sitting by the door"
+        
+        recs[os.path.join(path, file).replace('\\', '/')] = txt
 
 clips = []
 
 for i in range(100):
     clips.append(random.choice(list(recs.items())))
 
-test = [clips[i][1] for i in range(len(clips))]
+test = [clip[1] for clip in clips]
+print(test)
